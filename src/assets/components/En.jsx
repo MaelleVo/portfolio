649eab54f452b8e83dec2af89e203f49d459c2d8
MaelleVo <maellevolz@gmail.com>
2024-09-18 20:35:38 +0200

    update

--- src/assets/components/En.jsx
+++ src/assets/components/En.jsx
@@ -16,9 +16,10 @@ const En = () => {
           <article>
             <h2>Welcome.</h2>
             <p>
-              I am a Full Stack developer based in New-Aquitaine, France. <br />{" "}
-              I have completed several projects web & mobile during my bootcamp
-              at Le Reacteur. <br /> Mostly in React & React Native.
+              I am a Full Stack Developer based in New-Aquitaine, France. I have
+              completed several web and mobile projects during my bootcamp at Le
+              Reacteur, primarily using React and React Native. <br /> Mostly in
+              React & React Native.
             </p>
           </article>
           <img alt="illustration" src={illu}></img>
@@ -39,11 +40,12 @@ const En = () => {
 
               <h4>Website Marvel</h4>
               <p>
-                A project to create and implement a full stack website. The
-                functionalities are, the display from an API of the universe of
-                Marvel, caracters, comics. You can register, login and log out.
-                Also add favorites with cookies, search for a comics. The
-                project is made with React, Axios, Express.js & Mongo Atlas.
+                A project to create and implement a full-stack website. The
+                functionalities include displaying data from the Marvel API,
+                such as characters and comics. Users can register, log in, and
+                log out. Additionally, they can add favorites using cookies,
+                search for comics, and more. The project was built with React,
+                Axios, Express.js, and MongoDB Atlas.
               </p>
               <div className="link">
                 <a href="https://marvel-test-tech.netlify.app/" target="_blank">
@@ -65,10 +67,10 @@ const En = () => {
 
               <h4>Website Vinted</h4>
               <p>
-                Reproduction of the website vinted in full stack. All the
-                products are from a database. Register, log in or out
-                functionalities. Search into products also create new listings.
-                Payment done with Stripe.
+                Reproduction of the Vinted website in full-stack. All products
+                are sourced from a database. Users can register, log in, and log
+                out. The site allows searching for products and creating new
+                listings. Payments are processed using Stripe.
               </p>
               <div className="link">
                 <a
@@ -93,9 +95,9 @@ const En = () => {
 
               <h4>App Mobile</h4>
               <p>
-                Realisation of a mobile app for a client. It was a short group
-                project using react native. Functionalities to register, log in
-                and out, swip to vote.
+                Development of a mobile app for a client. It was a short group
+                project using React Native. The app includes functionalities
+                such as user registration, login and logout, and swipe to vote.
               </p>
               <div className="link"></div>
             </article>
@@ -107,8 +109,8 @@ const En = () => {
 
               <h4>Website Deliveroo</h4>
               <p>
-                Little project to reproduct one page for display the products,
-                adding them to our cart and calculate the total amount.
+                Small project to replicate a page displaying products, allowing
+                users to add them to the cart and calculate the total amount.
               </p>
               <div className="link">
                 <a
